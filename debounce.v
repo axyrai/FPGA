747c732800109c4fe1da41c727a3337dbba977a1
AkshayJRai <107136628+axyrai@users.noreply.github.com>
2023-04-16 15:09:10 +0530

    Update debounce.v

--- debounce.v
+++ debounce.v
@@ -1,14 +1,16 @@
 module debounce_ckt(
 input button,
 input clk,
-output reg result
+output wire result
     );
-  
+
+   
 wire Q1;
 wire Q2;
 wire EN1 = 1'b1;
 wire EN2 = 1'b1;
 wire xor_out;
+/****************************** Debounce ckt Implementation code ****************************************/
 
 DFF FF1 (button,clk,EN1,Q1);
 DFF FF2 (Q1,clk,EN2,Q2);
@@ -16,49 +18,57 @@ DFF FF2 (Q1,clk,EN2,Q2);
 xor g1 (xor_out,Q1,Q2);
 
 counter C1 (clk,xor_out,~Cout,Cout);     
-DFF2 FF3 (Q2,clk,Cout,result);
+DFF FF3 (Q2,clk,Cout,result);
 
 
 
 endmodule
 
 
-//N bit counter
- module counter (clk, SCLR,EN,Cout);
+/***************************************** N-bit counter *************************************/
+ module counter (clk, SCLR,EN,c);
      input clk;
      input SCLR;   // Clear of counter //
      input EN ; // Active 'HIGH' Enable //
-     output reg [N-1:0] Cout; // Counter Output //
+      reg [N-1:0] Cout; 
+	  output reg c;// Counter Output //
  
-         
-parameter N = 16;
+
+
+parameter N = 20;
 
             always@(posedge clk)
-                    if(SCLR)  
-						  Cout <= 0;
+                    if(SCLR) 
+							begin 						  
+							Cout <= 0;
+							c<=0;
+							end
                         else if (EN)    
                             begin
                                     if (Cout == N-1) 
+												begin
                                             Cout <= 0;
+														  c<=1;
+														  end
                                     else
+												begin
                                             Cout <= Cout + 1;
+														  c<=0;
                             end
+									 end
                     
 endmodule       
  
 
-//D flipflop
+/************************************* D Flip Flop Module (with Enable)**************************/
 
-module DFF(input D,input clk,input EN ,output wire Q);
-reg out;
+module DFF(input D,input clk,input EN ,output reg Q);
+
             always @(posedge clk ) 
                     begin
                         if(EN)
                                 begin
-                                   out <= D; 
+                                   Q <= D; 
                                 end 
-										   out <=out;	
-                    end 
-						  assign Q=out;
-					  
-endmodule 
+                    end     
+endmodule
